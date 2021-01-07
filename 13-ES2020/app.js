866001c3af3be31ee5e375357bb19d79d2a65ca7
simmy <simmy@freshcode.me>
2021-01-07 23:13:54 +0900

    padStart, padEnd

--- 13-ES2020/app.js
+++ 13-ES2020/app.js
@@ -1,16 +1,7 @@
-const me = {
-  name: 'simmy',
-  profile: {
-    email: '@gmail.com'
-  }
-}
+const today = new Date();
+// padStart와 padEnd는 String type에만 사용 가능하기 때문에 형변환이 필요하다.
+let hours = String(today.getHours()).padStart(2, '0').padEnd(3, 'h');
+let minutes = String(today.getMinutes()).padStart(2, '0').padEnd(3, 'm');
+let seconds = String(today.getSeconds()).padStart(2, '0').padEnd(3, 's');
 
-console.log(me.profile.email); // @gmail.com
-
-const someone = {
-  name: 'who',
-}
-
-console.log(someone.profile.email); // Uncaught TypeError: Cannot read property 'email' of undefined 에러 발생
-
-console.log(someone?.profile?.email?.domain);
+console.log(`${hours}:${minutes}:${seconds}`); // 12h:3m:15s 출력
