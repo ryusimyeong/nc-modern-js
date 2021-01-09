b372942899a521839c31012518147e575c9e032d
simmy <simmy@freshcode.me>
2021-01-09 17:02:52 +0900

    20210109

--- 13-ES2020/app.js
+++ 13-ES2020/app.js
@@ -1,7 +1,11 @@
-const today = new Date();
-// padStart와 padEnd는 String type에만 사용 가능하기 때문에 형변환이 필요하다.
-let hours = String(today.getHours()).padStart(2, '0').padEnd(3, 'h');
-let minutes = String(today.getMinutes()).padStart(2, '0').padEnd(3, 'm');
-let seconds = String(today.getSeconds()).padStart(2, '0').padEnd(3, 's');
+const obj = {
+  name: 'smsm',
+  age: 12
+}
 
-console.log(`${hours}:${minutes}:${seconds}`); // 12h:3m:15s 출력
+const arr = [['good', 'person'], ['name','hihi'], ['age', 34]]
+
+console.log(Object.fromEntries(arr))
+
+console.log(Object.keys(obj));
+console.log(Object.values(obj));
