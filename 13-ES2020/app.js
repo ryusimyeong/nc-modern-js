5e75b920cb4173a442bdbd2dc4eec993511e8dbc
simmy <simmy@freshcode.me>
2021-01-20 23:42:31 +0900

    array sort

--- 13-ES2020/app.js
+++ 13-ES2020/app.js
@@ -1,16 +1,2 @@
-let array = [1, [2], [[3]], [[[4]]], [[[[5]]]]];
-
-console.log(array); // [1, [2], [[3]], [[[4]]], [[[[5]]]]];
-
-array = array.flat(1);
-
-console.log(array); // [1, 2, [3], [[4]], [[[5]]]];
-
-array = array.flat(2);
-
-console.log(array); // [1, 2, 3, 4, [5]];
-
-array = array.flat(3);
-
-console.log(array); // [1, 2, 3, 4, 5];
+const test = ['a', 'b', 'c'];
 
